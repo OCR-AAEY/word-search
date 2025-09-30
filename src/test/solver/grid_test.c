d9a6ece02d5b1ca078b79bccc4a5d72ede2674f3
lomination <lomination@rejna.org>
2025-10-01 00:14:43 +0200

    Add tests for naive_solver.c

--- src/test/solver/grid_test.c
+++ src/test/solver/grid_test.c
@@ -2,7 +2,7 @@
 
 #include "solver/grid.h"
 
-Test(solver, load_grid_1) {
+Test(grid, load_grid_1) {
     Grid* grid = load_grid("./src/test/solver/grid_1.txt");
     cr_assert_arr_eq(grid_content(grid),
                      "HORIZONTALDXRAHCLBGADIKCILEOKCIGAJHYLYHIHGFGODTIOTGDLROWK"
@@ -12,17 +12,17 @@ Test(solver, load_grid_1) {
     free_grid(grid);
 }
 
-Test(solver, load_grid_2, .exit_code = 1) {
+Test(grid, load_grid_2, .exit_code = 1) {
     Grid * grid = load_grid("./this_file_is_not_supposed_to_exist");
     (void)grid;
 }
 
-Test(solver, load_grid_3, .exit_code = 1) {
+Test(grid, load_grid_3, .exit_code = 1) {
     Grid * grid = load_grid("./src/test/solver/grid_2.txt");
     (void)grid;
 }
 
-Test(solver, load_grid_4, .exit_code = 1) {
+Test(grid, load_grid_4, .exit_code = 1) {
     Grid * grid = load_grid("./src/test/solver/grid_3.txt");
     (void)grid;
 }
