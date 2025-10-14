5ab4da9311a0dcf4f1d90ba2b5993e2675402f34
Nota-yob <142342012+Nota-yob@users.noreply.github.com>
2025-10-14 20:30:17 +0200

    expose pretreatment internal function for test include

--- src/main/bounding_boxes/pretreatment.h
+++ src/main/bounding_boxes/pretreatment.h
@@ -11,4 +11,16 @@
 Matrix *gaussian_blur(Matrix *pixels, double sigma, size_t kernel_size);
 Matrix *image_to_grayscale(ImageData *img);
 ImageData *pixel_matrix_to_image(Matrix *matrix);
+
+/// ============= internal functions ===============
+
+uint8_t pixel_to_grayscale(Pixel *pixel);
+double gaussian_function(int x, int y, double sigma);
+Matrix *gaussian_kernel(Matrix *kernel, double sigma, size_t kernel_size);
+double sum_matrix_coefs(Matrix *mat);
+Matrix *scalar_multiplication(Matrix *mat, double a);
+Matrix *gaussian_normalize(Matrix *g);
+int clamp(int value, int min, int max);
+Matrix *gaussian_convolute(Matrix *g, Matrix *image, size_t kernel_size);
+
 #endif
