787a10561e81a4a9ba516c9daabad31703bfadce
Marionnette <brunoethan6@gmail.com>
2025-10-17 13:20:14 +0200

    Still broken

---
+++ src/main/image_loader/image_loading.h
@@ -0,0 +1,26 @@
+#ifndef IMAGE_LOADING_H
+
+#define IMAGE_LOADING_H
+
+#include <gdk-pixbuf/gdk-pixbuf.h>
+#include <glib.h>
+#include <stdint.h>
+
+typedef struct {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} Pixel;
+
+typedef struct {
+    int width;
+    int height;
+    Pixel *pixels;
+} ImageData;
+
+ImageData *load_image(const char *path);
+int save_pixbuf_to_png(GdkPixbuf *pixbuf, char *filename, GError **error);
+void free_pixels(guchar *pixels, gpointer data);
+void free_image(ImageData *img);
+
+#endif
