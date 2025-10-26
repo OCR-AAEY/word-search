37ae5c151915ac9bfef0a02e07299a0a031fadee
achref.ben-abdallah <achref.ben-abdallah@epita.fr>
2025-10-26 22:34:47 +0100

    fomated

--- src/main/extract_char/extract_char.h
+++ src/main/extract_char/extract_char.h
@@ -1,6 +1,6 @@
 #ifndef EXTRACT_H
 #define EXTRACT_H
-#include <gdk-pixbuf/gdk-pixbuf.h>
 #include "matrix.h"
-void extarct (struct Matrix matrix , size_t x0 , size_t y0 , size_t x1 , size_t y1);
+#include <gdk-pixbuf/gdk-pixbuf.h>
+void extarct(struct Matrix matrix, size_t x0, size_t y0, size_t x1, size_t y1);
 #endif
