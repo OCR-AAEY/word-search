128e7a26f35f7ab19c0caaae8824da30579518ce
lomination <lomination@rejna.org>
2025-10-26 23:19:18 +0000

    Implement functions for random generation

---
+++ src/main/utils/shuffle_array.h
@@ -0,0 +1,11 @@
+#ifndef SHUFFLE_ARRAY_H
+#define SHUFFLE_ARRAY_H
+
+/// @brief Shuffles the elements of a double array in place using the Fisher-Yates algorithm.
+/// @param array Pointer to the array of doubles to be shuffled.
+/// @param length The number of elements in the array.
+/// @note This function modifies the original array.
+/// @throw Assumes that the array pointer is valid and length > 0.
+void shuffle_array(double *array, unsigned long length);
+
+#endif
