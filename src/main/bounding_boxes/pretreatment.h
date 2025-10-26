c886cfc85526a8d6a3c028dbf77ee9bef9f5cf23
Nota-yob <142342012+Nota-yob@users.noreply.github.com>
2025-10-27 00:12:17 +0100

    add hough transform to detect grid lines

--- src/main/bounding_boxes/pretreatment.h
+++ src/main/bounding_boxes/pretreatment.h
@@ -8,19 +8,53 @@
 #include "image_loader/image_loading.h"
 #include "matrix/matrix.h"
 
-enum Orientation
-{
-    Vertical,
-    Horizontal
-};
+#define DEG2RAD(deg) ((deg) * M_PI / 180.0)
 
-enum MorphTransform
+/// @brief Orientation of a line or object.
+typedef enum Orientation
 {
+    /// Vertical orientation.
+    Vertical,
+
+    /// Horizontal orientation.
+    Horizontal
+} Orientation;
+
+/// @brief Type of morphological transformation.
+typedef enum MorphTransform
+{
+    /// Erosion operation.
     Erosion,
+
+    /// Dilation operation.
     Dilation,
+
+    /// Opening operation (erosion followed by dilation).
     Opening,
+
+    /// Closing operation (dilation followed by erosion).
     Closing
-};
+} MorphTransform;
+
+/// @brief Represents a line in polar coordinates.
+typedef struct Line
+{
+    /// The distance from the origin to the line.
+    double r;
+
+    /// The angle of the line in degrees.
+    double theta;
+} Line;
+
+/// @brief Represents a Point in a Matrix.
+typedef struct Point
+{
+    /// The coordinate on the height axis.
+    size_t h;
+
+    /// The coordinate on the width axis.
+    size_t w;
+} Point;
 
 Matrix *gaussian_blur(const Matrix *src, double sigma, size_t kernel_size);
 
@@ -39,6 +73,9 @@ Matrix *dilation(const Matrix *src, size_t kernel_size);
 Matrix *morph_transform(Matrix *src, size_t kernel_size,
                         enum MorphTransform transform);
 
+Line **hough_transform_lines(Matrix *src, float theta_precision, double delta_r,
+                             double delta_theta, size_t *size_out);
+
 /// ============= internal functions ===============
 
 uint8_t pixel_to_grayscale(Pixel *pixel);
@@ -52,4 +89,6 @@ int clamp(int value, int min, int max);
 Matrix *morph_transformation_1d(const Matrix *src, size_t kernel_size,
                                 enum MorphTransform transform,
                                 enum Orientation orientation);
+void print_lines(Line **lines, size_t size /*, size_t offset*/);
+
 #endif
