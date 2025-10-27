0e0ea0722b9165c8773dea7a7466bf1d2db988a2
Marionnette <brunoethan6@gmail.com>
2025-10-27 13:03:46 +0100

    Clang format 3

--- src/main/extract_char/extract_char.h
+++ src/main/extract_char/extract_char.h
@@ -1,7 +1,9 @@
 #ifndef EXTRACT_H
 #define EXTRACT_H
+
 #include <gdk-pixbuf/gdk-pixbuf.h>
 #include "../matrix/matrix.h"
 
 void extarct(const Matrix *matrix, size_t x0, size_t y0, size_t x1, size_t y1);
+
 #endif
