c8027e09cbf2b4e849d6fe83f0a65c03bb9fe745
lomination <lomination@rejna.org>
2025-10-27 12:06:53 +0000

    Make shuffle_array generic

--- src/main/utils/shuffle_array.c
+++ src/main/utils/shuffle_array.c
@@ -1,17 +1,25 @@
 #include <stdlib.h>
+#include <string.h>
 
 #include "random.h"
 #include "shuffle_array.h"
 
-void shuffle_array(double *array, unsigned long length)
+void shuffle_array(void *array, size_t elt_size, size_t length)
 {
-    for (unsigned long i = 0; i < length - 1; i++)
+    void *tmp = malloc(elt_size);
+    if (tmp == NULL)
+        errx(1, "Failed to allocate memory for internal temporary buffer.");
+
+    for (size_t i = 0; i < length - 1; i++)
     {
         // A random index after i.
-        unsigned long j = rand_ul_uniform_nm(i + 1, length - 1);
-        // Swap the ith and jth elements.
-        double tmp = array[j];
-        array[j] = array[i];
-        array[i] = tmp;
+        size_t j = rand_ul_uniform_nm(i + 1, length - 1);
+
+        // Swap the ith and jth elements (copy bytewise)
+        memcpy(tmp, array + j * elt_size, elt_size);
+        memcpy(array + j * elt_size, array + i * elt_size, elt_size);
+        memcpy(array + i * elt_size, tmp, elt_size);
     }
+
+    free(tmp);
 }
