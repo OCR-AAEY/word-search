bc1307d3d6ee1c281b3f8c6d0cfbbd1b12197667
lomination <lomination@rejna.org>
2025-10-27 14:40:10 +0000

    Add sigmoid functions in utils/

---
+++ src/main/utils/sigmoid.h
@@ -0,0 +1,13 @@
+#ifndef SIGMOID_H
+
+/// @brief Computes the sigmoid activation of a given input.
+/// @param x The input value.
+/// @return The sigmoid of the input.
+double sigmoid(double x);
+
+/// @brief Computes the derivative of the sigmoid function at a given input.
+/// @param x The input value.
+/// @return The derivative of the sigmoid function at the input.
+double sigmoid_derivative(double x);
+
+#endif
