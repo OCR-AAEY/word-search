79a56489af8582b98de2954b79b42f03ebfd7803
Nota-yob <142342012+Nota-yob@users.noreply.github.com>
2025-10-28 05:00:51 +0100

    drawing functions for visualizing lines and points

--- src/main/bounding_boxes/visualization.c
+++ src/main/bounding_boxes/visualization.c
@@ -4,40 +4,110 @@
 #include <gtk/gtk.h>
 #include <math.h>
 
-void draw_point(cairo_t *cr, double x, double y)
+void draw_point(cairo_t *cr, double x, double y, double radius)
 {
-    cairo_arc(cr, x, y, 100, 0, 2 * M_PI);
-    cairo_stroke(cr);
+    cairo_arc(cr, x, y, radius, 0, 2 * M_PI);
+    cairo_fill(cr);
+}
+
+void draw_points_on_img(Point **points, size_t points_count, char *filename)
+{
+    cairo_t *cr;
+    cairo_surface_t *surface;
+    surface = cairo_image_surface_create_from_png(filename);
+    if (cairo_surface_status(surface) != CAIRO_STATUS_SUCCESS)
+        errx(EXIT_FAILURE, "Failed to load the image with cairo");
+    cr = cairo_create(surface);
+
+    cairo_set_source_rgb(cr, 0, 1, 0);
+    for (size_t i = 0; i < points_count; i++)
+    {
+        draw_point(cr, points[i]->x, points[i]->y, 7);
+    }
+
+    char *result_filename = malloc(128 * sizeof(char));
+    sprintf(result_filename, "points_%s", filename);
+    cairo_surface_write_to_png(surface, result_filename);
+    free(result_filename);
+    cairo_destroy(cr);
+    cairo_surface_destroy(surface);
 }
 
 void draw_line(cairo_t *cr, Line *line, int width, int height)
 {
-    int offset = (int)ceil(sqrt(height * height + width * width));
-
     double theta = line->theta;
     double r = line->r;
+    // if (r < 0)
+    // {
+    //     r = -r;
+    //     theta = fmod(theta + 180, 360);
+    // }
+    double ct = cosd(theta);
+    double st = sind(theta);
 
-    double x_0 = r * cosd(theta);
-    double y_0 = r * sind(theta);
-    draw_point(cr, x_0, y_0);
-    printf("For line (%f, %f) : x_0 = %f, y_0 = %f\n", r, theta, x_0, y_0);
-    double dx = x_0;
-    double dy = -y_0;
+    // Possible intersections
+    double x[2], y[2];
+    int i = 0;
 
-    // Normalize direction to avoid huge coordinates
-    double norm = sqrt(dx * dx + dy * dy);
-    dx /= norm;
-    dy /= norm;
+    // Left border (x=0)
+    if (fabs(st) > 1e-9)
+    {
+        double y0 = r / st;
+        // printf("left border : y = %f, height = %i\n", y0, height);
+        if (y0 >= 0 && y0 <= height)
+        {
+            x[i] = 0;
+            y[i] = y0;
+            i++;
+        }
+    }
 
-    double x_1 = x_0 + offset * dx;
-    double y_1 = y_0 + offset * dy;
+    // Right border (x=W)
+    if (fabs(st) > 1e-9)
+    {
+        double y1 = (r - width * ct) / st;
+        // printf("right border : y = %f, height = %i\n", y1, height);
+        if (y1 >= 0 && y1 <= height)
+        {
+            x[i] = width;
+            y[i] = y1;
+            i++;
+        }
+    }
 
-    double x_2 = x_0 - offset * dx;
-    double y_2 = y_0 - offset * dy;
+    // Top border (y=0)
+    if (fabs(ct) > 1e-9)
+    {
+        double x0 = r / ct;
+        // printf("top border : x = %f, width = %i\n", x0, width);
+        if (x0 >= 0 && x0 <= width)
+        {
+            x[i] = x0;
+            y[i] = 0;
+            i++;
+        }
+    }
 
-    cairo_move_to(cr, x_1, y_1);
-    cairo_line_to(cr, x_2, y_2);
-    printf("Line : (%.2f, %.2f) -> (%.2f, %.2f)\n", x_1, y_1, x_2, y_2);
+    // Bottom border (y=H)
+    if (fabs(ct) > 1e-9)
+    {
+        double x1 = (r - height * st) / ct;
+        // printf("bottom border : x = %f, width = %i\n", x1, width);
+        if (x1 >= 0 && x1 <= width)
+        {
+            x[i] = x1;
+            y[i] = height;
+            i++;
+        }
+    }
+    // printf("cos = %f, sin = %f, theta = %f, r = %f\n", ct, st, theta, r);
+    if (i != 2)
+        errx(EXIT_FAILURE, "2 points not found, got %i, cannot draw line", i);
+    cairo_move_to(cr, x[0], y[0]);
+    cairo_line_to(cr, x[1], y[1]);
+    cairo_stroke(cr);
+
+    // printf("Draw (%.2f, %.2f) -> (%.2f, %.2f)\n", x[0], y[0], x[1], y[1]);
 }
 
 void draw_lines_on_img(Line **lines, size_t line_count, char *filename)
@@ -53,12 +123,12 @@ void draw_lines_on_img(Line **lines, size_t line_count, char *filename)
     int height = cairo_image_surface_get_height(surface);
 
     cairo_set_source_rgb(cr, 1, 0, 0);
+    cairo_set_line_width(cr, 5);
     for (size_t i = 0; i < line_count; i++)
     {
         draw_line(cr, lines[i], width, height);
-        cairo_set_line_width(cr, 2);
-        cairo_stroke(cr);
     }
+
     char *result_filename = malloc(128 * sizeof(char));
     sprintf(result_filename, "lines_%s", filename);
     cairo_surface_write_to_png(surface, result_filename);
--- src/main/bounding_boxes/visualization.h
+++ src/main/bounding_boxes/visualization.h
@@ -1,3 +1,4 @@
 #include "bounding_boxes/hough_lines.h"
 
+void draw_points_on_img(Point **points, size_t points_count, char *filename);
 void draw_lines_on_img(Line **lines, size_t line_count, char *filename);
