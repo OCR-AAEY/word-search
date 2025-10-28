38b38c81381c97cedcfcdc8d7cd7bc6920efcbd5
Nota-yob <142342012+Nota-yob@users.noreply.github.com>
2025-10-28 05:00:21 +0100

    fixed hough lines + testing

--- src/main/bounding_boxes/location.c
+++ src/main/bounding_boxes/location.c
@@ -10,8 +10,9 @@ int main()
     // ImageData *img = load_image("assets/test_images/montgolfiere.jpg");
     // ImageData *img = load_image("assets/sample_images/level_1_image_1.png");
     // ImageData *img = load_image("assets/sample_images/level_1_image_2.png");
-    ImageData *img = load_image("assets/sample_images/level_2_image_1.png");
-    // ImageData *img = load_image("assets/sample_images/level_2_image_2.png");
+    // ImageData *img = load_image("assets/sample_images/level_2_image_1.png");
+    ImageData *img = load_image("assets/sample_images/level_2_image_2.png");
+    // ImageData *img = load_image("untitled.png");
 
     Matrix *gray = image_to_grayscale(img);
     // ImageData *gray_img = pixel_matrix_to_image(gray);
@@ -27,22 +28,22 @@ int main()
     //  g_object_unref(pixbuf_blur);
     //  free_image(blured_img);
 
-    Matrix *threshold = adaptative_gaussian_thresholding(gray, 255, 11, 10, 7);
+    Matrix *threshold = adaptative_gaussian_thresholding(gray, 255, 11, 10, 5);
     mat_free(gray);
 
-    Matrix *opening = morph_transform(threshold, 2, Opening);
-    mat_free(threshold);
+    // Matrix *opening = morph_transform(threshold, 2, Opening);
+    // mat_free(threshold);
 
-    Matrix *closing = morph_transform(opening, 2, Closing);
-    mat_free(opening);
+    // Matrix *closing = morph_transform(opening, 2, Closing);
+    // mat_free(opening);
 
     // size_t offset =
     //     (size_t)round(sqrt(mat_height(closing) * mat_height(closing) +
     //                        mat_width(closing) * mat_width(closing)));
     size_t nb_lines;
-    Line **lines = hough_transform_lines(closing, 1, 3, 1, &nb_lines);
+    Line **lines = hough_transform_lines(threshold, 1, 5, 1, &nb_lines);
 
-    ImageData *result_img = pixel_matrix_to_image(closing);
+    ImageData *result_img = pixel_matrix_to_image(threshold);
     GdkPixbuf *pixbuf_result = create_pixbuf_from_image_data(result_img);
     save_pixbuf_to_png(pixbuf_result, "result.png", NULL);
     g_object_unref(pixbuf_result);
@@ -51,16 +52,20 @@ int main()
     free_image(img);
 
     // mat_free(blured);
-    mat_free(closing);
+    mat_free(threshold);
 
-    // print_lines(lines, nb_lines);
+    print_lines(lines, nb_lines);
 
     draw_lines_on_img(lines, nb_lines, "result.png");
 
     size_t nb_points;
     Point **points = extract_intersection_points(lines, nb_lines, &nb_points);
-    print_points(points, nb_points);
 
+    draw_points_on_img(points, nb_points, "lines_result.png");
+
+    print_points(points, nb_points);
+    free_points(points, nb_points);
+    free_lines(lines, nb_lines);
     return EXIT_SUCCESS;
 }
 
