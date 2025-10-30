f5fc5ee9516044380120715db508c51e93ec5983
Marionnette <brunoethan6@gmail.com>
2025-10-30 18:26:17 +0100

    Doxygen DocString

--- src/main/rotation/rotation.h
+++ src/main/rotation/rotation.h
@@ -5,7 +5,30 @@
 #include "matrix/matrix.h"
 #include "bounding_boxes/pretreatment.h"
 
-// Rotate an ImageData by the given angle (in degrees)
+/**
+ * @brief Rotates a grayscale matrix by a specified angle.
+ *
+ * This function rotates the given source matrix by the specified angle in degrees.
+ * The resulting matrix represents the rotated image in grayscale.
+ *
+ * @param src Pointer to the source Matrix to rotate.
+ * @param angle Rotation angle in degrees. Positive values rotate clockwise.
+ * @return Pointer to a new Matrix containing the rotated data.
+ *         Returns NULL on failure (e.g., memory allocation error).
+ */
 Matrix *rotate_matrix(const Matrix *src, double angle);
+
+/**
+ * @brief Rotates a full color image by a specified angle.
+ *
+ * This function rotates the given ImageData structure by the specified angle in degrees.
+ * The resulting ImageData is a newly allocated image containing the rotated pixels.
+ *
+ * @param img Pointer to the source ImageData to rotate.
+ * @param angle Rotation angle in degrees. Positive values rotate clockwise.
+ * @return Pointer to a new ImageData containing the rotated image.
+ *         Returns NULL on failure (e.g., memory allocation error).
+ */
 ImageData *rotate_image(ImageData *img, double angle);
+
 #endif
