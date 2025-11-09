550c1881172609b9e286764a1fe1c106c6e3052b
lomination <lomination@rejna.org>
2025-11-09 16:54:05 +0000

    Rewrite solver lib

--- src/main/solver/grid.h
+++ src/main/solver/grid.h
@@ -1,20 +1,82 @@
 #ifndef GRID_H
 #define GRID_H
 
+#include <stdlib.h>
+
 typedef struct Grid Grid;
 
-Grid *load_grid(char *file_name);
-
-const char *grid_content(Grid *grid);
-
+/// @brief Returns the height of the given Grid struct.
+/// @param[in] grid The struct of the sought height.
+/// @return The height of the given Grid struct.
 size_t grid_height(Grid *grid);
 
+/// @brief Returns the width of the given Grid struct.
+/// @param[in] grid The struct of the sought width.
+/// @return The width of the given Grid struct.
 size_t grid_width(Grid *grid);
 
-char get_char(Grid *grid, size_t height, size_t width);
+/// @brief Returns a pointer to the character at the specified position in the
+/// grid. This function does not perform bounds checking on the provided
+/// indices. It assumes that the caller ensures valid values for `h` and `w`.
+/// @param[in] grid Pointer to the grid structure.
+/// @param[in] h The row index (0-based).
+/// @param[in] w The column index (0-based).
+/// @return A pointer to the character located at position (`h`, `w`) in the
+/// grid.
+char *grid_unsafe_char_ptr(Grid *grid, size_t h, size_t w);
 
-void free_grid(Grid *grid);
+/// @brief Returns a pointer to the character at the specified position in the
+/// grid, performing bounds checking to ensure safe access.
+/// @param[in] grid Pointer to the grid structure to access.
+/// @param[in] h The row index (0-based).
+/// @param[in] w The column index (0-based).
+/// @return A pointer to the character located at position (`h`, `w`) in the
+/// grid.
+/// @throw Exits the program with EXIT_FAILURE if `h` or `w` are out of bounds.
+/// This function calls `errx()` internally to report invalid indices.
+char *grid_char_ptr(Grid *grid, size_t h, size_t w);
 
-void print_grid(Grid *grid);
+/// @brief Returns the character at the specified position in the grid,
+/// performing bounds checking to ensure safe access.
+/// @param[in] grid Pointer to the grid structure to access.
+/// @param[in] h The row index (0-based).
+/// @param[in] w The column index (0-based).
+/// @return The character located at position (`h`, `w`) in the grid.
+/// @throw Exits the program with EXIT_FAILURE if `h` or `w` are out of bounds.
+/// This function calls `errx()` internally to report invalid indices.
+char grid_char(Grid *grid, size_t h, size_t w);
+
+/// @brief Loads a grid from a given file and returns a pointer to the created
+/// Grid struct. The returned Grid must be freed using `free_grid`.
+/// @param[in] file_name The file to load as a word search grid.
+/// @return Returns the created grid.
+/// @throw Throws if any error occurs during the process including: file not
+/// found, invalid char in file, grid too small, failed (re)allocation.
+Grid *grid_load_from_file(char *file_name);
+
+/// @brief Frees the data of the given grid as well as the given Grid pointer
+/// itself.
+/// @param[in] grid The grid to free.
+void grid_free(Grid *grid);
+
+/// @brief Prints the grid in the standard output.
+/// @param[in] grid The pointer to the grid to print.
+void grid_print(Grid *grid);
+
+/// @brief Solve a word search problem using a naive algorithm (not optimized).
+/// @param[in] grid The grid to solve.
+/// @param[in] word The word to search in the grid. Note that it has to be upper
+/// case.
+/// @param[out] start_height The height of the start point of the word if it is
+/// found. Otherwise set to -1.
+/// @param[out] start_width The width of the start point of the word if it is
+/// found. Otherwise set to -1.
+/// @param[out] end_height The height of the end point of the word if it is
+/// found. Otherwise set to -1.
+/// @param[out] end_width The width of the end point of the word if it is found.
+/// Otherwise set to -1.
+/// @return Returns 1 if the sought word has been found, 0 otherwise.
+int grid_solve(Grid *grid, char *word, int *start_height, int *start_width,
+               int *end_height, int *end_width);
 
 #endif
