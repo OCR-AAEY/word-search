0ffda619c1ade145adc55fb7c461995f7df3da63
lomination <lomination@rejna.org>
2025-12-01 17:21:01 +0000

    Format code

--- src/test/test_settings.h
+++ src/test/test_settings.h
@@ -4,7 +4,8 @@
 #define TEST_SETTINGS_H
 
 /** The precesion with which float are compared using cr_*_float_eq. */
-#define epsilon(expected) (absval(expected) > 1 ? 1E-3f * absval(expected) : 1E-3f)
+#define epsilon(expected)                                                      \
+    (absval(expected) > 1 ? 1E-3f * absval(expected) : 1E-3f)
 
 #ifndef TEST_ITERATION
 #define TEST_ITERATION 100
@@ -12,6 +13,7 @@
 
 /** The number of times random test will be repeated. */
 #define REPEAT                                                                 \
-    for (size_t repeat_counter = 0; repeat_counter < TEST_ITERATION; repeat_counter++)
+    for (size_t repeat_counter = 0; repeat_counter < TEST_ITERATION;           \
+         repeat_counter++)
 
 #endif
