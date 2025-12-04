33f0e4deade0fbdb615b34882d2d7893c162e9c1
Nota-yob <142342012+Nota-yob@users.noreply.github.com>
2025-12-04 19:46:59 +0100

    YAFFC : forgot to format my bad i wont (oh yes i will) redo this again

--- src/main/location/split_letters.c
+++ src/main/location/split_letters.c
@@ -1,6 +1,7 @@
 #include "location/split_letters.h"
 
-unsigned int get_average_letter_width(BoundingBox ***letters_boxes, size_t nb_words, size_t *word_nb_letters)
+unsigned int get_average_letter_width(BoundingBox ***letters_boxes,
+                                      size_t nb_words, size_t *word_nb_letters)
 {
     unsigned int total_letters = 0;
     unsigned int sum = 0;
@@ -8,7 +9,8 @@ unsigned int get_average_letter_width(BoundingBox ***letters_boxes, size_t nb_wo
     {
         for (size_t letter = 0; letter < word_nb_letters[word]; letter++)
         {
-            sum += letters_boxes[word][letter]->br.x - letters_boxes[word][letter]->tl.x;
+            sum += letters_boxes[word][letter]->br.x -
+                   letters_boxes[word][letter]->tl.x;
             total_letters++;
         }
     }
@@ -37,38 +39,51 @@ BoundingBox **split_large_letter(BoundingBox *letter_box, size_t nb_parts)
         new_letter_box->br.x = new_letter_box->tl.x + split_letter_width;
         new_letter_boxes[i] = new_letter_box;
     }
-    new_letter_boxes[nb_parts-1]->br.x = letter_box->br.x;
+    new_letter_boxes[nb_parts - 1]->br.x = letter_box->br.x;
     return new_letter_boxes;
 }
 
-BoundingBox ***detect_split_large_letters(BoundingBox ***letters_boxes, size_t nb_words, size_t *word_nb_letters)
+BoundingBox ***detect_split_large_letters(BoundingBox ***letters_boxes,
+                                          size_t nb_words,
+                                          size_t *word_nb_letters)
 {
-    unsigned int avg_letter_width = get_average_letter_width(letters_boxes, nb_words, word_nb_letters);
+    unsigned int avg_letter_width =
+        get_average_letter_width(letters_boxes, nb_words, word_nb_letters);
     unsigned int letter_width;
     for (size_t word = 0; word < nb_words; word++)
     {
         for (size_t letter = 0; letter < word_nb_letters[word]; letter++)
         {
-            letter_width = letters_boxes[word][letter]->br.x - letters_boxes[word][letter]->tl.x;
+            letter_width = letters_boxes[word][letter]->br.x -
+                           letters_boxes[word][letter]->tl.x;
             if (letter_width / avg_letter_width >= 2)
             {
                 size_t nb_parts = (size_t)(letter_width / avg_letter_width);
-                BoundingBox **split_boxes = split_large_letter(letters_boxes[word][letter], nb_parts);
+                BoundingBox **split_boxes =
+                    split_large_letter(letters_boxes[word][letter], nb_parts);
                 size_t new_word_size = word_nb_letters[word] + nb_parts - 1;
-                BoundingBox **new_word = malloc(new_word_size * sizeof(BoundingBox *));
-                for (size_t prev_old_letters_index = 0; prev_old_letters_index < letter; prev_old_letters_index++)
+                BoundingBox **new_word =
+                    malloc(new_word_size * sizeof(BoundingBox *));
+                for (size_t prev_old_letters_index = 0;
+                     prev_old_letters_index < letter; prev_old_letters_index++)
                 {
-                    new_word[prev_old_letters_index] = letters_boxes[word][prev_old_letters_index];
+                    new_word[prev_old_letters_index] =
+                        letters_boxes[word][prev_old_letters_index];
                 }
 
-                for (size_t new_letters_index = 0; new_letters_index < nb_parts; new_letters_index++)
+                for (size_t new_letters_index = 0; new_letters_index < nb_parts;
+                     new_letters_index++)
                 {
-                    new_word[letter + new_letters_index] = split_boxes[new_letters_index];
+                    new_word[letter + new_letters_index] =
+                        split_boxes[new_letters_index];
                 }
 
-                for (size_t remaining_letters_index = letter + 1; remaining_letters_index < word_nb_letters[word]; remaining_letters_index++)
+                for (size_t remaining_letters_index = letter + 1;
+                     remaining_letters_index < word_nb_letters[word];
+                     remaining_letters_index++)
                 {
-                    new_word[letter + nb_parts + remaining_letters_index] = letters_boxes[word][remaining_letters_index];
+                    new_word[letter + nb_parts + remaining_letters_index] =
+                        letters_boxes[word][remaining_letters_index];
                 }
                 free(split_boxes);
                 free(letters_boxes[word]);
--- src/main/location/split_letters.h
+++ src/main/location/split_letters.h
@@ -4,10 +4,14 @@
 #include "location/location.h"
 
 // TODO : BFS to make connectivity checks on letters and split if not connected
-// TODO : Average size of letters and if letter > 2*avg, get the number of avg letters is has and divide by this number the box - Check
+// TODO : Average size of letters and if letter > 2*avg, get the number of avg
+// letters is has and divide by this number the box - Check
 
-unsigned int get_average_letter_width(BoundingBox ***letters_boxes, size_t nb_words, size_t *word_nb_letters);
+unsigned int get_average_letter_width(BoundingBox ***letters_boxes,
+                                      size_t nb_words, size_t *word_nb_letters);
 BoundingBox **split_large_letter(BoundingBox *letter_box, size_t nb_parts);
-BoundingBox ***detect_split_large_letters(BoundingBox ***letters_boxes, size_t nb_words, size_t *word_nb_letters);
+BoundingBox ***detect_split_large_letters(BoundingBox ***letters_boxes,
+                                          size_t nb_words,
+                                          size_t *word_nb_letters);
 
 #endif
